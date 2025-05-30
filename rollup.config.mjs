881bc40fd055f32f81fcbe6481e7a3274c6f0d18
MaxHuang <hc18174419502@163.com>
2025-05-30 17:20:33 +0800

    finished rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,30 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 export default {
-    input: 'src/main.js',
-    output: {
-        file: 'bundle.js',
-        format: 'cjs'
-    }
-}
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js", // CommonJS 格式
+      format: "cjs",
+      exports: "auto",
+    },
+    {
+      file: "dist/index.esm.js", // ES Module 格式
+      format: "esm",
+    },
+  ],
+  Plugin: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+    }),
+    terser(), //代码压缩
+  ],
+  external: ["crypto-js"], // 外部依赖
+};
